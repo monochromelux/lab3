e4c6dfa3e32adca94436e47ab0861b0cb3c3ef85
Hyoeun Lee <ggrr12345@naver.com>
2018-11-22 04:11:32 +0900

    4번 수정, 5번&7번 추가

---
+++ 5/project5.c
@@ -0,0 +1,21 @@
+﻿#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void main(){
+  pid_t pid;
+  printf("fork call\n");
+  pid = fork();
+
+  if (pid == 0) {
+    printf("child process!\n");
+  }
+
+  else if (pid > 0) {
+    printf("parent process!\n");
+  }
+
+  else{
+    printf("fork error\n");
+  }
+}
